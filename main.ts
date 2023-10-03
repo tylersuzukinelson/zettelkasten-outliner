25017c0bef9162f4061745f057659b2ca147a425
Tyler Suzuki Nelson <tyler@tylersuzukinelson.com>
2023-10-02 18:36:59 -0700

    Add basic functionality for generating an outline

--- main.ts
+++ main.ts
@@ -1,85 +1,61 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface ZettelkastenOutlinerSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: ZettelkastenOutlinerSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ZettelkastenOutliner extends Plugin {
+	settings: ZettelkastenOutlinerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'zettelkasten-outliner-create-outline',
+			name: 'Create Zettelkasten Outline',
+			callback: async () => {
+				const currentFile = this.app.workspace.getActiveFile();
+				if (currentFile) {
+					const outputFile = await this.app.vault.create(`Zettelkasten Outline ${new Date().getTime()}.md`, "");
+					this.parseZettel(outputFile, currentFile, 0);
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
+	}
+
+	getChildrenFiles(file: TFile): TFile[] {
+		let children = []	as TFile[];
+		const linkToFile = `[[${file.path.replace(/\.md$/, "")}]]`;
+		this.app.vault.getMarkdownFiles().forEach((markdownFile) => {
+			if (this.app.metadataCache.getFileCache(markdownFile)?.frontmatter?.parent === linkToFile) {
+				children.push(markdownFile);
+			}
+		});
+		return children;
+	}
+
+	parseZettel(outputFile: TFile, zettel: TFile, indentationLevel: number) {
+		this.app.vault.append(outputFile, this.generateListItem(zettel, indentationLevel));
+		this.getChildrenFiles(zettel).forEach((child) => {
+			this.parseZettel(outputFile, child, indentationLevel + 1);
+		});
+	}
+
+	generateListItem(file: TFile, indentationLevel: number): string {
+		let identation = "";
+		for (let i = 0; i < indentationLevel; i++) {
+			identation = identation.concat("  ");
+		}
+
+		return `${identation}- [[${file.path.replace(/\.md$/, "")}]]\n`;
 	}
 
 	async loadSettings() {
@@ -88,47 +64,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
